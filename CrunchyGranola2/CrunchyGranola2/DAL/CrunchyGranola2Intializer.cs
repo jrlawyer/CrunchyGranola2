c2058d2d70f3a712abbb4ddc690f2e5a92d19eb5
Jennifer <jrlawyer@gmail.com>
2016-08-09 10:48:25 -0400

    I finished the seed data.

--- CrunchyGranola2/CrunchyGranola2/DAL/CrunchyGranola2Intializer.cs
+++ CrunchyGranola2/CrunchyGranola2/DAL/CrunchyGranola2Intializer.cs
@@ -17,6 +17,35 @@ namespace CrunchyGranola2.DAL
                 new Customer {CustomerID = 3, FirstName = "Stephanie", LastName = "Lawyer", DateOfLastPurchase=DateTime.Parse("2016-08-01")},
                 new Customer {CustomerID = 4, FirstName = "Anthony", LastName = "Gage", DateOfLastPurchase=DateTime.Parse("2016-08-01")}
             };
+
+            customers.ForEach(c => context.Customers.Add(c));
+            context.SaveChanges();
+
+            var products = new List<Product>
+            {
+                new Product {ProductID = 10, Description = "Handmade Scarf", Price = 15, UpcCode = 1234567, LeadTime = "2 months", Quantity = 1},
+                new Product {ProductID = 11, Description = "Organic Apples", Price = 1, UpcCode = 1234568, LeadTime = "1 week", Quantity = 5},
+                new Product {ProductID = 12, Description = "Funky Hat", Price = 10, UpcCode = 1234569, LeadTime = "3 months", Quantity = 1},
+                new Product {ProductID = 13, Description = "Ocean Caught Salmon", Price = 8, UpcCode = 1234570, LeadTime = "1 week", Quantity = 2},
+                new Product {ProductID = 14, Description = "Expensive Vitamins", Price = 50, UpcCode = 1234571, LeadTime = "1 month", Quantity = 3},
+            };
+
+            products.ForEach(p => context.Products.Add(p));
+            context.SaveChanges();
+
+            var purchase = new List<Purchase>
+            {
+                new Purchase {PurchaseID = 100, CustomerID = 1, ProductID = 11 },
+                new Purchase {PurchaseID = 100, CustomerID = 1, ProductID = 13 },
+                new Purchase {PurchaseID = 101, CustomerID = 2, ProductID = 10 },
+                new Purchase {PurchaseID = 101, CustomerID = 2, ProductID = 14 },
+                new Purchase {PurchaseID = 102, CustomerID = 3, ProductID = 14 },
+                new Purchase {PurchaseID = 103, CustomerID = 4, ProductID = 12 },
+
+            };
+
+            purchase.ForEach(u => context.Purchases.Add(u));
+            context.SaveChanges();
         }
 
     }
