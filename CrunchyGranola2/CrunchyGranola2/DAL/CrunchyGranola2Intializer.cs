c89eb6fb067b59320023d5621382e6bca5980ff9
Jennifer <jrlawyer@gmail.com>
2016-08-09 10:31:21 -0400

    I created the context for the database and began the seed data.

---
+++ CrunchyGranola2/CrunchyGranola2/DAL/CrunchyGranola2Intializer.cs
@@ -0,0 +1,23 @@
+﻿using CrunchyGranola2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrunchyGranola2.DAL
+{
+    public class CrunchyGranola2Intializer : System.Data.Entity. DropCreateDatabaseIfModelChanges<CrunchyGranola2Context>
+    {
+        protected override void Seed(CrunchyGranola2Context context)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {CustomerID = 1, FirstName = "James", LastName = "Alexander", DateOfLastPurchase=DateTime.Parse("2016-08-01")},
+                new Customer {CustomerID = 2, FirstName = "Jennifer", LastName = "Lawyer", DateOfLastPurchase=DateTime.Parse("2016-08-01")},
+                new Customer {CustomerID = 3, FirstName = "Stephanie", LastName = "Lawyer", DateOfLastPurchase=DateTime.Parse("2016-08-01")},
+                new Customer {CustomerID = 4, FirstName = "Anthony", LastName = "Gage", DateOfLastPurchase=DateTime.Parse("2016-08-01")}
+            };
+        }
+
+    }
+}
