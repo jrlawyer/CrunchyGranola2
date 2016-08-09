a736f1ea4381a53ea49c89b5f9bbca9edc49dcb8
Jennifer <jrlawyer@gmail.com>
2016-08-09 10:15:40 -0400

    I added the entities and updated the cshtml files.

---
+++ CrunchyGranola2/CrunchyGranola2/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrunchyGranola2.Models
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]    
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public int UpcCode { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string LeadTime { get; set; }
+
+        public virtual ICollection<Purchase>Purchases { get; set; }
+    }
+}
