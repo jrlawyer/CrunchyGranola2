15271a1dfac9df1d035d5dcfae92c511fa4a5627
Jennifer <jrlawyer@gmail.com>
2016-08-10 11:51:20 -0400

    I added the department and employee models.'

---
+++ CrunchyGranola2/CrunchyGranola2/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrunchyGranola2.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set;}
+
+        public int DepartmentID { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstName; }
+        }
+
+        public virtual Department Department { get; set; }
+
+
+    }
+}
